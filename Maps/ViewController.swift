128f66f610d43c10e742560f4a964f8f965a8657
Yawo Echitey <Yawo@yawomac.local>
2016-12-05 21:59:30 -0200

    Inicial Commit

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let lat: CLLocationDegrees = -5.8271441
+        let long: CLLocationDegrees = -35.2205344
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let coord = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        let region = MKCoordinateRegion(center: coord, span: span)
+        
+        map.setRegion(region, animated: true)
+        
+        let listener = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        
+        map.addGestureRecognizer(listener)
+        
     }
-
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer){
+        let point = gestureRecognizer.location(in: self.map)
+        
+        let coord = map.convert(point, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "\(coord.latitude)  \(coord.longitude)"
+        annotation.subtitle = "Here you clicked!"
+        annotation.coordinate = coord
+        map.addAnnotation(annotation)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
